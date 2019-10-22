df7ac654365d72fb79fab0afc15f701e68c1f6df
GribanovaM <56697233+GribanovaM@users.noreply.github.com>
2019-10-22 19:19:33 +0300

    Add files via upload

--- oldcoortonew.f95
+++ oldcoortonew.f95
@@ -1,15 +1,14 @@
 program oldctonew
 implicit none
 
-integer i,n,m,ier,ninp
-real nl, nls, nF, nD, nOm
-real(8), allocatable, dimension(:,:) ::  al 
+integer i,n,m,ier,ninp,var,k
+real nl, nls, nF, nD, nOm, pi,q
+real(8), allocatable, dimension(:,:) ::  al , b
 real(8), allocatable, dimension(:) ::  P1
 integer, allocatable, dimension(:) ::  l,ls,F,D,Om
 integer, allocatable, dimension(:) ::  moon, sun, omega, w
-data ninp / 11 /
-open(unit=ninp,file='tab5.2a.txt',status='old',form='formatted')
-open(12,file='tab5.2a.new.txt',status='new',form='formatted')
+
+pi= 4* atan(1.0)
 
 nl=1717915923.2178
 nls=129596581.0481
@@ -20,6 +19,10 @@ nOm=-6962890.5431
 !кол-во столбцов и строк
 n=1600
 m=17
+k=4
+
+
+
 allocate(moon(n), stat=ier)
 if (ier/=0) stop
 allocate(sun(n), stat=ier)
@@ -30,6 +33,10 @@ allocate(w(n), stat=ier)
 if (ier/=0) stop
 allocate(al(n,m), stat=ier)
 if (ier/=0) stop
+allocate(b(n,k), stat=ier)
+if (ier/=0) stop
+
+
 allocate(l(n), stat=ier)
 if (ier/=0) stop
 allocate(ls(n), stat=ier)
@@ -42,6 +49,18 @@ allocate(Om(n), stat=ier)
 if (ier/=0) stop
 allocate(P1(n), stat=ier)
 if (ier/=0) stop
+
+
+write(*,*) "Введите: 1 - от старых к новым, 2 - новые к старым"
+read(*,*) var
+
+
+select case (var)
+
+case(1)
+
+open(11,file='tab5_2a.txt',status='old',form='formatted')
+open(12,file='tab5_2a.new.txt', form='formatted')
 
 
    do i=1,n
@@ -57,20 +76,51 @@ Om(:)= int (al(:,8))
 
 do i=1,1600,1
    moon(i) = F(i)+D(i)+Om(i)
-   sun(i) = -D(i)
-   omega(i) = -l(i)
-   w(i) = Om(i)-F(i)-l(i)
+   sun(i) = F(i) + Om(i)
+   omega(i) = Om(i)
+   w(i) = D(i)+F(i)-ls(i)
+enddo
+
+
+
+do i=1,n
+   P1(i)=(pi*2*206265*100)/((l(i)*nl+ls(i)*nls+F(i)*nF+D(i)*nD+Om(i)*nOm))
 enddo
 
 
 do i=1,n
-   P1(i)=(3.1415926535897931*2*206265*100)/((l(i)*nl+ls(i)*nls+F(i)*nF+D(i)*nD+Om(i)*nOm))
+  write(12,*) moon(i), sun(i), omega(i), w(i)
 enddo
 
-write(12,*) 'Lambda_moon	Lambda_sun	omega   	w'
-do i=1,n
-  write(12,*) moon(i), sun(i), omega(i), w(i), P1(i)
+case(2)
+
+open(22,file='tab5_2a.new.txt',status= 'old', form='formatted')
+open(13,file='tab5_2a.test.txt',form='formatted')
+
+
+   do i=1,n,1
+      read(22,*) b(i,:)
+!      write(*,*) al(i,:)
+   enddo
+
+moon(:)= int (b(:,1))
+sun(:)=int (b(:,2))
+omega(:)=int (b(:,3))
+w(:)=int (b(:,4))
+
+do i=1,n,1
+   l(i) = 0 !я хз что тут
+   ls(i) =  moon(i) - omega(i) - w(i)
+   F(i) = moon(i)-omega(i)
+   D(i) = moon(i)-sun(i)
+   Om(i) = omega(i)
 enddo
+
+do i=1,n
+  write(13,*) l(i), ls(i), F(i), D(i), Om(i)
+enddo
+
+end select
 
 
 end
